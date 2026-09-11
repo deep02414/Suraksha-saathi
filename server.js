ef9df8e558ec3cd51f85b998aeda47c04f1571b3
deep02414 <deepsingh02414@gmail.com>
2026-09-12 03:31:39 +0530

    Fix variable name from _dirname to __dirname

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
